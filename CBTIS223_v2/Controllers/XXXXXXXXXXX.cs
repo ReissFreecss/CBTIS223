e1bfa55634ec448a8089dbeab76874459a7683cf
CARLOS PEREZ CALDERON <L19091404@zacatepec.tecnm.mx>
2023-11-06 22:31:40 -0600

    Agregue controler XXXXXXXXXXXX

---
+++ CBTIS223_v2/Controllers/XXXXXXXXXXX.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CBTIS223_v2.Controllers
+{
+    public class XXXXXXXXXXX : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
