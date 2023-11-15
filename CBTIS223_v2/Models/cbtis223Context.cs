8dd10d1d3d9c734f193354bb774b2cae28f64949
CARLOS PEREZ CALDERON <L19091404@zacatepec.tecnm.mx>
2023-11-15 15:11:48 -0600

    .

--- CBTIS223_v2/Models/cbtis223Context.cs
+++ CBTIS223_v2/Models/cbtis223Context.cs
@@ -36,7 +36,7 @@ namespace CBTIS223_v2.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("server=localhost;port=3306;database=cbtis223;user=root;password=root", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql"));
+                optionsBuilder.UseMySql("server=localhost;port=3306;database=cbtis223;user=root;password=chocomilk", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql"));
             }
         }
 
