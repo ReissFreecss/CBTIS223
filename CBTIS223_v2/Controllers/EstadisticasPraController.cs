e855b568e8acad5c1399a13d453cf8a757d4a073
CHRISTOPHER REBOLLEDO CABALLERO <L20090290@zacatepec.tecnm.mx>
2023-11-23 10:08:08 -0600

    Se agrego las estadisticas para practicas profesionales y la pestaña para su navegacion en cada vista.

--- CBTIS223_v2/Controllers/EstadisticasPraController.cs
+++ CBTIS223_v2/Controllers/EstadisticasPraController.cs
@@ -8,12 +8,12 @@ using LicenseContext = OfficeOpenXml.LicenseContext;
 
 namespace CBTIS223_v2.Controllers
 {
-    public class EstadisticasController : Controller
+    public class EstadisticasPraController : Controller
     {
-        public IActionResult ExportToExcelEspe(string CicloForm,string Espe)
+        public IActionResult ExportToExcelEspe(string CicloForm, string Espe)
         {
             // Obtiene los datos que deseas exportar (usando la consulta SQL modificada)
-            var data = GetDataForExportEspe(CicloForm,Espe);
+            var data = GetDataForExportEspe(CicloForm, Espe);
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
             // Crea un nuevo paquete de Excel
@@ -42,8 +42,8 @@ namespace CBTIS223_v2.Controllers
                 worksheet.Cells["D1"].Value = "CantidadAlumnos";
 
                 // Aplica un formato de negrita y un fondo de color gris al título
-               
-               // titleCell.Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+
+                // titleCell.Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
                 //titleCell.Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.Gray);
 
                 // Llena los datos en el archivo Excel
@@ -54,7 +54,7 @@ namespace CBTIS223_v2.Controllers
                     worksheet.Cells[$"B{row}"].Value = item.Especialidad;
                     worksheet.Cells[$"C{row}"].Value = item.InstitucionServicio;
                     worksheet.Cells[$"D{row}"].Value = item.CantidadAlumnos;
-                    
+
                     row++;
                 }
 
@@ -65,20 +65,23 @@ namespace CBTIS223_v2.Controllers
                     stream.Position = 0;
 
                     // Devuelve el archivo Excel como una descarga
-                    return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "ServicioSocialData.xlsx");
+                    return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "PracticasProfesionalesData.xlsx");
                 }
             }
         }
 
 
-        public List<ExportData> GetDataForExportEspe(string CicloForm,string Espe)
+        public List<ExportData> GetDataForExportEspe(string CicloForm, string Espe)
         {
             using (var dbContext = new cbtis223Context())
             {
-                var data = dbContext.ServicioSocials
-                    .Where(ss => ss.EstudianteNcNavigation.Ciclo == CicloForm && ss.EstudianteNcNavigation.Especialidad ==Espe)
-                    .GroupBy(ss => new { ss.IdInstiServicioNavigation.Institucion, ss.EstudianteNcNavigation.Ciclo,
-                        ss.EstudianteNcNavigation.Especialidad })
+                var data = dbContext.PracticasProfesionales
+                    .Where(ss => ss.EstudianteNcNavigation.EstudianteNcNavigation.Ciclo == CicloForm && ss.EstudianteNcNavigation.EstudianteNcNavigation.Especialidad == Espe)
+                    .GroupBy(ss => new {
+                        ss.IdInstiPracticasNavigation.Institucion,
+                        ss.EstudianteNcNavigation.EstudianteNcNavigation.Ciclo,
+                        ss.EstudianteNcNavigation.EstudianteNcNavigation.Especialidad
+                    })
                     .Select(group => new ExportData
                     {
                         InstitucionServicio = group.Key.Institucion,
@@ -107,9 +110,18 @@ namespace CBTIS223_v2.Controllers
 
 
                 // Agrega los encabezados de las columnas
+                worksheet.Cells["A1"].Style.Font.Bold = true;
+                worksheet.Cells["A1"].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                worksheet.Cells["A1"].Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.Cyan);
                 worksheet.Cells["A1"].Value = "Ciclo Escolar";
+                worksheet.Cells["B1"].Style.Font.Bold = true;
+                worksheet.Cells["B1"].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                worksheet.Cells["B1"].Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.Cyan);
                 worksheet.Cells["B1"].Value = "Institución";
-                worksheet.Cells["C1"].Value = "Cantidad de Alumnos";
+                worksheet.Cells["C1"].Style.Font.Bold = true;
+                worksheet.Cells["C1"].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                worksheet.Cells["C1"].Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.Cyan);
+                worksheet.Cells["C1"].Value = "CantidadAlumnos";
 
                 // Llena los datos en el archivo Excel
                 var row = 2;
@@ -121,7 +133,7 @@ namespace CBTIS223_v2.Controllers
                     row++;
                 }
 
-   
+
                 // Guarda el paquete en un MemoryStream
                 using (var stream = new MemoryStream())
                 {
@@ -139,11 +151,11 @@ namespace CBTIS223_v2.Controllers
         {
             using (var dbContext = new cbtis223Context())
             {
-                var data = dbContext.ServicioSocials
-                    .Where(ss => ss.EstudianteNcNavigation.Ciclo == CicloForm && ss.IdInstiServicio == Insti)
+                var data = dbContext.PracticasProfesionales
+                    .Where(ss => ss.EstudianteNcNavigation.EstudianteNcNavigation.Ciclo == CicloForm && ss.IdInstiPracticas == Insti)
                     .GroupBy(ss => new {
-                        ss.IdInstiServicioNavigation.Institucion,
-                        ss.EstudianteNcNavigation.Ciclo,
+                        ss.IdInstiPracticasNavigation.Institucion,
+                        ss.EstudianteNcNavigation.EstudianteNcNavigation.Ciclo,
                     })
                     .Select(group => new ExportData
                     {
